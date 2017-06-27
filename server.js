f78f9345a94084d5f3274a4098202ac8e6d87f8c
Joshua S <joshua.salom1996@gmail.com>
2017-06-26 18:57:38 -0700

    start api setup

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-//server
+//server.js
+
+var express = require('express');
+var app     = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;
+
+//routes
+var router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Found API!'});
+});
+
+//api prefix
+app.use('/api', router);
+
+app.listen(port);
+console.log('Server running on port ' + port);
