180d26372f149012ae5f406ba77ee3d8af0dd16b
Joshua S <joshua.salom1996@gmail.com>
2017-07-30 00:01:16 -0700

    v1.0: scrim page done

--- server.js
+++ server.js
@@ -3,14 +3,11 @@
 var express = require('express');
 var app     = express();
 var bodyParser = require('body-parser');
-//var mongoose = require('mongoose');
 var path = require('path');
 var MongoClient = require('mongodb');
 var ObjectID = require('mongodb').ObjectID;
-//import {MongoClient, ObjectID} from 'mongodb';
 var assert = require('assert');
 
-//mongoose.connect('mongodb://localhost/quarrelofcuties');
 let mdb;
 MongoClient.connect('mongodb://localhost:27017/quarrelofcuties', (err, db) => {
   assert.equal(null, err);
@@ -26,14 +23,13 @@ app.use(express.static(path.resolve(__dirname, './public')));
 
 var port = process.env.PORT || 3000;
 
-//var Animal = require('./src/models/animal');
 
 //routes
 var router = express.Router();
 
 router.use(function(req, res, next) {
   //do logging
-  console.log('Something is happening.');
+  console.log(req.method + " request for " + req.url);
   next();
 });
 
@@ -46,18 +42,6 @@ router.route('/animals')
 //post func
   .post(function(req, res) {
     res.json('You aren\'t allowed to create an animal');
-  /*
-    var animal = new Animal();
-    animal.species = req.body.species;
-    animal.elo = req.body.elo;
-    
-    //save model
-    animal.save(function(err) {
-      if (err)
-        res.send(err);
-      res.json({message: 'Animal created'});
-    });
-    */
   })
 //get all animals func
   .get(function(req, res) {
@@ -80,13 +64,6 @@ router.route('/animals')
         }
         animals[animal._id] = animal;
       });
-    /*
-    Animal.find(function(err, animals) {
-      if (err)
-        res.send(err);
-      res.json(animals);
-    });
-    */
   });
 
 router.route('/animals/:animal_id')
@@ -104,48 +81,25 @@ router.route('/animals/:animal_id')
         }
         animals[animal._id] = animal;
       });
-    /*
-    Animal.findById(req.params.animal_id, function(err, animal) {
-      if (err)
-        res.send(err);
-      res.json(animal);
-    });
-    */
   })
 //update animal func
   .put(function(req, res) {
-    mdb.collection('reddit').findOneAndUpdate(
+    mdb.collection('reddit').updateOne(
       {_id: ObjectID(req.params.animal_id)}, 
-      {$set: {elo: req.body.elo}},
+      {
+        $set: {elo: req.body.elo},
+        $inc: {gamesPlayed: 1}
+      },
       {upsert: true},
       function(err, doc){
         assert.equal(null, err);
         res.json(doc);
+        console.log(doc);
       });
   //db.inventory.findOneAndUpdate( {item: 'postcard'}, {$set: {elo:1500}, $inc: {games: 1}, {upsert: true}})
   })
-  /*
-    Animal.findById(req.params.animal_id, function(err, animal) {
-      if (err)
-        res.send(err);
-      animal.species = req.body.species;
-      animal.elo = req.body.elo;
-      animal.save(function(err) {
-        if (err)
-          res.send(err);
-        res.json({message: 'Animal updated'});
-      });
-    });
-    */
   .delete(function(req, res) {
     res.json('You aren\'t allowed to delete an animal');
-    /*
-    Animal.remove({_id:req.params.animal_id}, function(err, animal) {
-      if (err)
-        res.send(err);
-      res.json({message: 'Animal deleted'});
-    });
-    */
   });
 
 router.route('/scrim') //get two scrim partners
@@ -155,41 +109,13 @@ router.route('/scrim') //get two scrim partners
       {$match:{}},
       {$project: {_id:1, author:1, score:1, url:1, elo:1, gamesPlayed:1}},
       {$sort:{gamesPlayed:1}},
-      {$limit:10},
+      {$limit:3},
       {$sample:{size:2}}
-      ])
+    ])
       .toArray(function(err, scrimArray) {
         assert.equal(null, err);
         res.json({scrimArray});
       });
-  /*
-    mdb.collection('reddit').find({})
-      .sort({gamesPlayed:1})
-      .limit(2)
-      .project({
-        _id: 1,
-        author: 1,
-        score: 1,
-        url: 1,
-        elo: 1,
-        gamesPlayed: 1
-      })
-      .toArray(function(err, scrimArray) {
-        assert.equal(null, err);
-        res.json({scrimArray});
-      });
-      */
-    /*
-      .each((err, animal) => {
-        assert.equal(null, err);
-    
-        if(!animal) { //no more animals
-          res.json({scrimList});
-          return;
-        }
-        scrimList[animal._id] = animal;
-      });
-      */
   });
 
 /////////////////////////////////////////////////
--- src/components/Scrim.js
+++ src/components/Scrim.js
@@ -8,15 +8,17 @@ class Scrim extends React.Component {
     this.state = {
       scrimmers: [],
       buttonVisible: false,
-      selectedIndex: "blank",
+      selectedIndex: 'blank',
+      notSelectedIndex: 'blank',
       currentScrimmers: []
     };
-    
-    //this.changeSelection = this.handleClick.bind(this);
-    //this.alertMessage = this.alertMessage.bind(this);
   }
   
   componentDidMount() {
+    this.getScrimPartners();
+  }
+  
+  getScrimPartners(){
     axios.get('/api/scrim')
       .then(res => {
         const scrimmers = res.data.scrimArray.map(function(animal) {return animal;});
@@ -29,17 +31,52 @@ class Scrim extends React.Component {
   }
   
   changeSelection(animalIndex) {
+    var notSelectedAnimalIndex = '';
+    for (var i=0; i < this.state.currentScrimmers.length; i++){
+      if (this.state.currentScrimmers[i] !== animalIndex){
+        notSelectedAnimalIndex = this.state.currentScrimmers[i];
+      }
+    }
     this.setState({
       buttonVisible: true,
-      selectedIndex: animalIndex
-      
+      selectedIndex: animalIndex,
+      notSelectedIndex: notSelectedAnimalIndex
     });
   }
-  alertMessage(string){
+  alertMessage(string){ //()=>this.alertMessage(this.state.selectedIndex)
     alert(string);
   }
+  
+  confirmSelection(winner, loser) {
+    //TODO: calculate ELO
+    axios.put('/api/animals/' + winner, {
+      elo: 1000
+    })
+      .then(function(response) {
+        return response.data;
+      });
+    axios.put('/api/animals/' + loser, {
+      elo: 1000
+    })
+      .then(function(response) {
+        return response.data;
+      });
+    this.getScrimPartners();
+    this.setState({
+      buttonVisible: false,
+      selectedIndex: 'blank',
+      notSelectedIndex: 'blank'
+    });
+  }
+  
   render() {
-    const confirmButton = (this.state.buttonVisible? <button className='buttonLockIn' onClick={()=>this.alertMessage(this.state.selectedIndex)}>Confirm that Cutie</button> : null);
+    const confirmButton = 
+      (this.state.buttonVisible? 
+        <button className='buttonLockIn' 
+          onClick={()=>this.confirmSelection(this.state.selectedIndex, this.state.notSelectedIndex)}>
+        Confirm that Cutie
+        </button> 
+        : null);
     return (
       <div>
         <h2>
@@ -48,7 +85,7 @@ class Scrim extends React.Component {
         <div>
           {this.state.scrimmers.map(animal => 
             <div className={this.state.selectedIndex===animal._id ? 'scrimProfileA':'scrimProfileI'} key={animal._id} 
-                           onClick={()=>this.changeSelection(animal._id)}> 
+              onClick={()=>this.changeSelection(animal._id)}> 
               <div>Picture by {animal.author}</div>
               <div className="imgFrame">
                 <span className="imgHelper"><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></span><img src={animal.url}/> {/*next-line spam to help vertical alignment because I don't know how to fix it*/}
